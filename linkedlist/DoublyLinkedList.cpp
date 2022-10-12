b0b46ba82545f1f5973590a7feb96a4932fa892e
Suyash Purwar <purwarsuyash06@gmail.com>
2022-10-13 02:21:48 +0530

    implement deleteAtTail, deleteAtHead, deleteAtPosition, and deleteOccureneceOf functions. Fix bugs in in insertAtHead, insertAtTail, insertAtPosition

--- linkedlist/DoublyLinkedList.cpp
+++ linkedlist/DoublyLinkedList.cpp
@@ -56,13 +56,21 @@ class DoublyLinkedList {
 
     void insertAtHead(int data) {
         Node *temp = new Node(data, NULL, head);
-        head -> previous = temp;
+        if (head) {
+            head -> previous = temp;
+        } else {
+            tail = temp;
+        }
         head = temp;
     }
 
     void insertAtTail(int data) {
         Node *temp = new Node(data, tail, NULL);
-        tail -> next = temp;
+        if (tail) {
+            tail -> next = temp;
+        } else {
+            head = temp;
+        }
         tail = temp;
     }
 
@@ -77,14 +85,95 @@ class DoublyLinkedList {
                 i++;
             }
             Node *new_node = new Node(data, temp, temp->next);
-            if (temp -> next) {
-                temp -> next -> previous = new_node;
-            }
+            if (temp -> next) temp -> next -> previous = new_node;
             temp -> next = new_node;
-            if (!new_node -> next) {
-                tail = new_node;
-            }
+            if (!new_node -> next) tail = new_node;
         }
+    }
+
+    int deleteAtHead() {
+        if (this -> length() == 0) {
+            cout << "Nothing to delete!" << endl;
+            return -1;
+        }
+        if (this -> length() == 1) {
+            int delete_data = head -> data;
+            delete head;
+            head = tail = NULL;
+            return delete_data;
+        }
+        Node *temp = head;
+        temp = temp -> next;
+        temp -> previous = NULL;
+        int deleted_data = head -> data;
+        delete head;
+        head = temp;
+        return deleted_data;
+    }
+
+    int deleteAtTail() {
+        if (this -> length() == 0) {
+            cout << "Nothing to delete!";
+            return -1;
+        }
+        if (this -> length() == 1) {
+            int delete_data = head -> data;
+            delete head;
+            head = tail = NULL;
+            return delete_data;
+        }
+        Node *temp = head;
+        while (temp -> next != tail) {
+            temp = temp -> next;
+        }
+        Node *to_be_deleted = temp -> next;
+        int deleted_data = to_be_deleted -> data;
+        temp -> next = NULL;
+        delete to_be_deleted;
+        tail = temp;
+        return deleted_data;
+    }
+
+    int deleteAtPosition(int position) {
+        if (this -> length() == 0) {
+            cout << "Nothing to delete!";
+            return -1;
+        }
+        if (position == 1) return this -> deleteAtHead();
+        int i = 1;
+        Node *before_target = head;
+        while (i < position-1) {
+            before_target = before_target -> next;
+            i++;
+        }
+        Node *target = before_target -> next;
+        Node *after_target = target -> next;
+        int deleted_data = target -> data;
+        before_target -> next = after_target;
+        if (after_target) after_target -> previous = before_target;
+        if (!before_target -> next) tail = before_target;
+        delete target;
+        return deleted_data;
+    }
+
+    int deleteOccurenceOf(int value) {
+        if (this -> length() == 0) {
+            cout << "Nothing to delete!" << endl;
+            return -1;
+        }
+        if (head -> data == value) return deleteAtHead();
+        Node *target_before = head;
+        while (target_before -> next -> data != value) {
+            target_before = target_before -> next;
+        }
+        Node *target = target_before -> next;
+        Node *target_after = target -> next;
+        int deleted_data = target -> data;
+        target_before -> next = target_after;
+        if (target_after) target_after -> previous = target_before;
+        if (!target_before -> next) tail = target_before;
+        delete target;
+        return deleted_data;
     }
 };
 
@@ -94,9 +183,47 @@ int main() {
     dll -> insertAtHead(3);
     dll -> insertAtHead(9);
     dll -> insertAtHead(4);
+    dll -> insertAtHead(42);
+    dll -> insertAtHead(10);
 
     dll -> traverse();
 
+    cout << dll -> deleteOccurenceOf(10) << endl;
+    dll -> traverse();
+    cout << "head: " << dll->head->data << " tail: " << dll->tail->data << endl;
+
+    // cout << dll -> deleteAtHead() << endl;
+    // cout << dll -> deleteAtHead() << endl;
+    // cout << dll -> deleteAtHead() << endl;
+    // cout << dll -> deleteAtHead() << endl;
+    // cout << dll -> deleteAtHead() << endl;
+    // cout << dll -> deleteAtHead() << endl;
+    // cout << dll -> deleteAtHead() << endl;
+    
+    // dll -> insertAtHead(12);
+    // dll -> insertAtTail(2);
+
+    // dll -> traverse();
+    // dll -> travsereReverse();
+
+    // cout << dll -> deleteAtPosition(1) << endl;
+    // dll -> traverse();
+
+    // cout << dll -> deleteAtPosition(5) << endl;
+    // dll -> traverse();
+    // cout << dll -> tail -> data << endl;
+    
+    // dll -> travsereReverse();
+
+    // cout << dll -> deleteAtHead() << endl;
+    // cout << dll -> head -> data << endl;
+    
+    // dll -> traverse();
+
+    // cout << dll -> deleteAtTail() << endl;
+    // cout << dll -> tail -> data << endl;
+
+    // dll -> traverse();
 
     // dll -> insertAtHead(2);
 
