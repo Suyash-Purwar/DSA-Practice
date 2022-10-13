11df4ec916bc56b3d1207d3fdf878f6fe5ad44e7
Suyash Purwar <purwarsuyash06@gmail.com>
2022-10-13 22:22:28 +0530

    Fix bug in deleteNode, update traversal, and add length function

--- linkedlist/CircularLinkedList.cpp
+++ linkedlist/CircularLinkedList.cpp
@@ -23,6 +23,17 @@ class CircularLinkedList {
         tail -> next = tail;
     }
 
+    int length() {
+        if (!tail) return 0;
+        Node *temp = tail;
+        int count = 1;
+        while (temp -> next != tail) {
+            temp = temp -> next;
+            count++;
+        }
+        return count;
+    }
+
     void insertNodeAfter(int data, int target) {
         if (tail) {
             Node* current = tail;
@@ -45,7 +56,10 @@ class CircularLinkedList {
 
     void traversal() {
         Node* current = tail;
-        if (!tail) return;
+        if (!tail) {
+            cout << "List is empty" << endl;
+            return;
+        }
         do {
             cout << current -> data << " ==> ";
             current = current -> next;
@@ -54,19 +68,25 @@ class CircularLinkedList {
     }
 
     int deleteNode(int value) {
-        if (tail -> next == tail) {
-            int deleted_data = tail -> data;
-            delete tail;
-            tail = NULL;
-            return deleted_data;
+        if (tail == NULL) {
+            cout << "List is empty" << endl;
+            return -1;
         }
+        bool is_iteration_complete = false;
         Node *before_target = tail;
-        while (before_target -> next -> data != value) {
+        while (before_target -> next -> data != value && !is_iteration_complete) {
             before_target = before_target -> next;
+            if (before_target == tail) is_iteration_complete = true;
+        }
+        if (is_iteration_complete) {
+            cout << "Target not found!" << endl;
+            return -1;
         }
         Node *target = before_target -> next;
         int deleted_data = target -> data;
         before_target -> next = target -> next;
+        if (before_target == target) tail = NULL;
+        if (target == tail) tail = before_target;
         delete target;
         return deleted_data;
     }
@@ -74,6 +94,17 @@ class CircularLinkedList {
 
 int main() {
     CircularLinkedList *cll = new CircularLinkedList(10);
+
+    cll -> insertNodeAfter(1, 10);
+    cll -> insertNodeAfter(13, 1);
+
+    cll -> traversal();
+
+    cout << cll -> deleteNode(10) << endl;
+
+    cll -> traversal();
+
+    cout << cll -> length() << endl;
 
     // cll -> insertNodeAfter(7, 10);
     // cll -> traversal();
@@ -87,9 +118,17 @@ int main() {
     // cll -> insertNodeAfter(101, 10);
     // cll -> traversal();
 
-    cout << cll -> deleteNode(7) << endl;
+    // cll -> insertNodeAfter(3, 10);
+    // cout << cll -> deleteNode(3) << endl;
 
-    cll -> traversal();
+    // cll -> traversal();
+    // cout << cll -> tail -> data << endl;
+
+    // cout << cll -> deleteNode(3) << endl;
+
+    // cll -> traversal();
+    // // cout << cll -> length() << endl;
+    // cout << cll -> deleteNode(3) << endl;
 
     return 0;
 }
