b1ffbabd2d0763e575bbe36ccd8da53194b190f2
Suyash Purwar <purwarsuyash06@gmail.com>
2022-10-26 20:04:34 +0530

    Push arrays9.cpp and minor fix in arrays8.cpp

--- arrays/arrays9.cpp
+++ arrays/arrays9.cpp
@@ -1,24 +1,17 @@
-/**
- * @name Pascal's Triangle (LeetCode Easy)
- * @brief Given an integer numRows, return the first numRows of Pascal's triangle.
- * @link https://leetcode.com/problems/pascals-triangle/
- * @author Suyash Purwar (github.com/suyash-purwar)
- */
-
 #include <iostream>
 #include <vector>
 
-using std::endl;
 using std::cout;
+using std::endl;
 using std::vector;
 
 class Solution {
     public:
-    vector<vector<int>> generate(int numRows) {
+    vector<int> getRow(int rowIndex) {
         vector<vector<int>> triangle = {{1}};
         vector<int> temp;
 
-        for (int m = 1; m < numRows; m++) {
+        for (int m = 1; m < rowIndex+1; m++) {
             int i = -1; int j = 0;
             for (int n = 0; n < m + 1; n++) {
                 if (i < 0) temp.push_back(triangle[m-1][j]);
@@ -30,18 +23,16 @@ class Solution {
             triangle.push_back(temp);
             temp.clear();
         }
-        return triangle;
+        return triangle[rowIndex];
     }
 };
 
 int main() {
     Solution sol;
-    vector<vector<int>> t1 = sol.generate(10);
-    cout << endl << endl;
-    for (int i = 0; i < t1.size(); i++) {
-        for (int j = 0; j < t1[i].size(); j++) {
-            cout << t1[i][j] << " ";
-        } cout << endl;
+    vector<int> r1 = sol.getRow(0);
+    for (int i = 0; i < r1.size(); i++) {
+        cout << r1[i] << " ";
     }
+    cout << endl;
     return 0;
 }
