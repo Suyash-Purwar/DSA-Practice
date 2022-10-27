d8b19b5047d33aa8f4293d8ed8e18b169490b4d8
Suyash Purwar <purwarsuyash06@gmail.com>
2022-10-27 23:35:01 +0530

    Push arrays11.cpp

---
+++ arrays/arrays11.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<vector>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::vector;
+
+class Solution {
+    public:
+    vector<vector<int>> transpose(vector<vector<int>> &matrix) {
+        vector<vector<int>> transposedMatrix;
+        vector<int> temp;
+        for (int i = 0; i < matrix[0].size(); i++) {
+            for (int l = 0; l < matrix.size(); l++) {
+                temp.push_back(matrix[l][i]);
+            }
+            transposedMatrix.push_back(temp);
+            temp.clear();
+        }
+        return transposedMatrix;
+    }
+};
+
+int main() {
+    Solution sol;
+    // vector<vector<int>> matrix = {
+    //     {2, 4, -1},
+    //     {-10, 5, 11},
+    //     {10, 14, -3}
+    // };
+    vector<vector<int>> matrix = {
+        {2, 4,},
+        {-10, 5},
+        {10, 14}
+    };
+    vector<vector<int>> transposedMatrix = sol.transpose(matrix);
+
+    for (int k = 0; k < transposedMatrix.size(); k++) {
+        for (int m = 0; m < transposedMatrix[k].size(); m++) {
+            cout << transposedMatrix[k][m] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
