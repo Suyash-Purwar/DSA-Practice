ffe7de4e843d8c360b27c1e05204d98b83f6f8a1
Suyash Purwar <purwarsuyash06@gmail.com>
2023-06-28 23:57:05 +0530

    Add comment

--- arrays/sort012.cpp
+++ arrays/sort012.cpp
@@ -1,3 +1,11 @@
+/**
+ * @file 75. Sort Colors (Leetcode Medium)
+ * @brief Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue. We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively. You must solve this problem without using the library's sort function.
+ * @link https://leetcode.com/problems/sort-colors/description/
+ * @author Suyash Purwar (github.com/suyash-purwar)
+ * @date 28-06-2023
+*/	
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
