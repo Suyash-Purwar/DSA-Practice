f8789bc3675b0f87181cfddac87432c5abcec186
Suyash Purwar <purwarsuyash06@gmail.com>
2023-08-21 20:08:53 +0530

    Add question info in majorityElement.cpp

--- arrays/majorityElement.cpp
+++ arrays/majorityElement.cpp
@@ -1,3 +1,11 @@
+/*
+ * @file 169. Majority Element (Leetcode Easy)
+ * @brief Given an array nums of size n, return the majority element. The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+ * @link https://leetcode.com/problems/majority-element
+ * @author Suyash Purwar (github.com/suyash-purwar)
+ * @date 20-07-2023
+*/
+
 #include <iostream>
 #include <vector>
 #include <unordered_map>
